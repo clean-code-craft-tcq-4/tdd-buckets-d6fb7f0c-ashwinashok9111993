9d1dd17794f8e73b2a6497efd6865d4c3f8fab3d
Ashwin Ashok (SX/EDC3) <Ashok.Ashwin@bosch.com>
2022-09-04 10:59:06 +0530

    Commit for TDD - Final

--- RangeReaderStack.Tests/RangeReaderClassTests.cs
+++ RangeReaderStack.Tests/RangeReaderClassTests.cs
@@ -1,14 +1,48 @@
 using System;
 using Xunit;
+using RangeReaderStack;
+using System.Collections.Generic;
 
 namespace RangeReaderStack.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestGetValueByCountInList()
         {
+            Assert.Equal(2, RangeReaderClass.getCountByValue(new List<int> { 1, 2, 3, 1, 2 }, 2));
+        }
 
+        [Fact]
+        public void TestgetRangeFromMinMax()
+        {
+            Assert.Equal(new List<int> { 1, 2, 3, 4 }, RangeReaderClass.getRangeFromMinMax(1, 4));
+        }
+
+        [Fact]
+        public void TestDetectRanges()
+        {
+            Assert.Equal(new Dictionary<int, int> { { 1, 4 }, { 6, 8 }, { 15, 17 } },
+                RangeReaderClass.DetectRanges(new List<int> { 1, 1, 2, 3, 4, 4, 6, 7, 8, 15, 16, 17 }));
+        }
+
+        [Fact]
+        public void TestCompareSequenceEquals()
+        {
+            Assert.True(RangeReaderClass.CompareSequenceEquals(new List<int> { 1, 1, 2, 3, 4, 4, 6, 7, 8, 15, 16, 17 }, 1, 4));
+            Assert.False(RangeReaderClass.CompareSequenceEquals(new List<int> { 1, 1, 2, 3, 4, 4, 6, 7, 8, 15, 16, 17 }, 1, 5));
+        }
+
+        [Fact]
+        public void TestGetCountInRangeAndList()
+        {
+            Assert.StrictEqual(4,RangeReaderClass.GetCountInRangeAndList(new List<int> { 1, 1, 1, 2 }, 1, 2));
+        }
+
+        [Fact]
+        public void TestRangeWriter()
+        {
+            Assert.Equal("1-4,6", RangeReaderClass.RangeWriter(new List<int> { 1, 1, 2, 3, 4, 4, 6, 7, 8, 15, 16, 17 },1,4));
         }
     }
 }
--- RangeReaderStack/RangeReaderClass.cs
+++ RangeReaderStack/RangeReaderClass.cs
@@ -1,8 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RangeReaderStack
 {
-    public class Class1
+    public class RangeReaderClass
     {
+        // Get the integer count by occurence in an input integer list
+        public static int getCountByValue(List<int> inputIntegerList, int targetValue)
+        {
+            return inputIntegerList.Where(x => x == targetValue).Count();
+        }
+
+        public static List<int> getRangeFromMinMax(int min,int max)
+        {
+            return Enumerable.Range(min, max - min + 1).ToList();
+        }
+
+        public static Dictionary<int,int> DetectRanges(List<int> inputList)
+        {
+            var distinctInputList = inputList.Distinct().ToList();
+            int primaryIterator = distinctInputList.Min();
+            int iterator = primaryIterator + 1;
+            Dictionary<int, int> rangeDict = new Dictionary<int, int>();
+
+            while (primaryIterator < distinctInputList.Max())
+            {
+
+                while (CompareSequenceEquals(distinctInputList, primaryIterator, iterator))
+                {
+                    rangeDict[primaryIterator] = iterator++;
+
+                }
+                primaryIterator = iterator;
+                iterator = primaryIterator + 1;
+            }
+
+            return rangeDict;
+        }
+
+        // Checks for the range in the given input integer list, returns true if present
+        public static bool CompareSequenceEquals(List<int> distinctInputList, int min, int max)
+        {
+            return Enumerable.SequenceEqual(getRangeFromMinMax(min, max)
+                               .Intersect(distinctInputList).ToList(),
+                                getRangeFromMinMax(min, max))
+                                && (max <= distinctInputList.Max())
+                                && (min <= max);
+        }
+
+        public static int GetCountInRangeAndList(List<int> InputList,int min,int max)
+        {
+            var count = 0;
+            foreach (int value in getRangeFromMinMax(min,max))
+            {
+                count = count + getCountByValue(InputList, value);
+            }
+            return count;
+        }
+
+        public static string RangeWriter(List<int> InputList,int min, int max)
+        {
+            return $"{min}-{max},{GetCountInRangeAndList(InputList,min,max)}";
+        }
+
+        public static void printTableFromInputList(List<int> InputList)
+        {
+            foreach(var input in DetectRanges(InputList))
+            {
+                Console.WriteLine(RangeWriter(InputList, input.Key, input.Value));
+            }
+        }
+        
     }
 }
